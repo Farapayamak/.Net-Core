2a6a284d7e34cd5668c8a9c3772d75f3a3355cfb
amhome <amirhosseinmehrvarzi@gmail.com>
2022-08-27 16:16:50 +0430

    RestClient and its samples

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace FaraPayamak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RestClient restClient = new RestClient("username", "password");
+            restClient.SendSMS("09123456789", "5000xxx", "test sms");
+            string credit = restClient.GetCredit().Value;
+            var result = restClient.BaseServiceNumber("test sms", "09123456789", 5555);
+
+            // SOAP samples are inside the QuickStart folder
+        }
+    }
+}
