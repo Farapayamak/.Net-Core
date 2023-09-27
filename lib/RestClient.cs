be16a836f733e40412c2536fc726b48849507d9a
amhome <amirhosseinmehrvarzi@gmail.com>
2023-09-28 00:09:44 +0330

    rest: add smart ws methods

--- lib/RestClient.cs
+++ lib/RestClient.cs
@@ -8,9 +8,10 @@ using System.Text;
 
 namespace FaraPayamak
 {
-    class RestClient
+    public class RestClient
     {
         private readonly string Endpoint = "https://rest.payamak-panel.com/api/SendSMS/";
+        private readonly string Endpoint_Smart = "https://rest.payamak-panel.com/api/SmartSMS/";
         private string Username { get; set; }
         private string Password { get; set; }
 
@@ -27,15 +28,33 @@ namespace FaraPayamak
             values.Add("username", Username);
             values.Add("password", Password);
 
+            string endpoint = operation.Contains("SmartSMS") ? Endpoint.Replace("SendSMS/", "") : Endpoint;
             using (var client = new WebClient())
             {
-                var response = client.UploadValues(Endpoint + operation, values.Aggregate(new NameValueCollection(),
+                var response = client.UploadValues(endpoint + operation, values.Aggregate(new NameValueCollection(),
                 (seed, current) => {
                     seed.Add(current.Key, current.Value);
                     return seed;
                 }));
 
                 return JsonConvert.DeserializeObject<RestResponse>(Encoding.Default.GetString(response));
+            }
+        }
+
+        
+        private string RequestAsJson(string operation, Dictionary<string, object> values)
+        {
+            values.Add("username", Username);
+            values.Add("password", Password);
+
+            using (var client = new WebClient())
+            {
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+
+                var data = JsonConvert.SerializeObject(values);
+                var response = client.UploadString(Endpoint_Smart + operation, data);
+
+                return response;
             }
         }
 
@@ -86,7 +105,47 @@ namespace FaraPayamak
                 { "text", text }, { "to", to }, { "bodyId", bodyId.ToString() }
             });
         }
+
+
+
+        public RestResponse SendSmartSMS(string to, string text, string from, string fromSupportOne, string fromSupportTwo)
+        {
+            string operation = "SmartSMS/Send";
+            return Request(operation, new Dictionary<string, string> {
+                { "to", to }, { "text", text }, { "from", from }, { "fromSupportOne", fromSupportOne }, { "fromSupportTwo", fromSupportTwo }
+            });
+        }
+
+        public RestSmartResponse SendMultipleSmartSMS(string[] to, string[] text, string from, string fromSupportOne, string fromSupportTwo)
+        {
+            string operation = "SendMultiple";
+            var result = RequestAsJson(operation, new Dictionary<string, object> {
+                { "to", to }, { "text", text }, { "from", from }, { "fromSupportOne", fromSupportOne }, { "fromSupportTwo", fromSupportTwo }
+            });
+            return JsonConvert.DeserializeObject<RestSmartResponse>(result);
+        }
+
+        public RestResponse GetSmartDeliveries2(long id)
+        {
+            string operation = "SmartSMS/GetDeliveries2";
+            return Request(operation, new Dictionary<string, string> {
+                { "Id", id.ToString() }
+            });
+        }
+
+        public RestResponse GetSmartDeliveries(long[] ids)
+        {
+            string operation = "GetDeliveries";
+            var result = RequestAsJson(operation, new Dictionary<string, object> {
+                { "Ids", ids }
+            });
+            return JsonConvert.DeserializeObject<RestResponse>(result);
+        }
+
     }
+
+
+
 
 
     public class RestResponse
@@ -94,5 +153,19 @@ namespace FaraPayamak
         public string Value { get; set; }
         public int RetStatus { get; set; }
         public string StrRetStatus { get; set; }
+    }
+
+
+    public class RestSmartResponse
+    {
+        public string ReqStatus { get; set; }
+        public string Message { get; set; }
+        public List<RestSmartResult> Result { get; set; }
+    }
+
+    public class RestSmartResult
+    {
+        public string Mobile { get; set; }
+        public long ID { get; set; }
     }
 }
